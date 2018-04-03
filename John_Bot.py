7310cc2e6a34095b22aca963a026db8cb170f163
SgtSafety <johnreilly1989@hotmail.co.uk>
2018-04-03 21:11:23 +0100

    Created a simple calculator which replaces the original add and sub functions. Uses python operators and check to make sure no data is enter which isnt an operator/bracket or a digit

--- John_Bot.py
+++ John_Bot.py
@@ -6,39 +6,69 @@ from base_discord_bot import Music
 
 
 class John_Bot(Music):
-
     @commands.command()
-    async def help(self):
+    async def info(self):
         await self.bot.say(
+            """
+            I can do the following commands\n
+            ?Calc : Standard Calculator (NOT Scientific, uses python operators)\n
+            """)
+
+    """
+    Below Is old redundant code for addition and sub traction. New method Calc does all the below and some more.
+
+    I use eval but do check that it is only mathimatical operators or numbers which are use, so hopefully hackers can't
+    destroy this program. Never say never though!
+    """
+
+    # @commands.command(pass_context=False, no_pm=True)
+    # async def add(self, *nums):
+    #     try:
+    #         result = sum(map(int, nums))
+    #     except:
+    #         await self.bot.say("Numbers only please")
+    #     await self.bot.say("{} = {}".format((' + '.join(map(str, list(nums)))), result))
+    #
+    # @commands.command(pass_context=False, no_pm=True)
+    # async def sub(self, *nums):
+    #     try:
+    #         nums = list(map(int, nums))
+    #         result = nums[0] - sum(nums[1:])
+    #     except:
+    #         await self.bot.say("Numbers only please")
+    #     await self.bot.say("{} = {}".format((' - '.join(map(str, list(nums)))), result))
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def MyEntrance(self, ctx, member: discord.Member, channel: discord.Channel):
         """
-        I can do the following commands\n
-        add : add n numbers (n1 + n2 + n3 .....)
-        sub : sub n numbers (n1 - n2 - n3 .....)
-        """)
+        Cannot get bot to move voice channel once it is connected to a voice channel.
+        Solution is to perhaps to disconnect bot after it plays the message.
+
+        """
+
+        # try:
+        #     voice = await bot.join_voice_channel(channel)
+        # except:
+        #     #sort this out!
+        #     #discord.VoiceClient.move_to(bot.get_channel(channel.id))
+        # await self.bot.move_member(member, channel)
+        # player = voice.create_ffmpeg_player('Ta Da.mp3')
+        # player.start()
+        pass
 
     @commands.command(pass_context=False, no_pm=True)
-    async def add(self, *nums):
-        result = 0
-        for num in nums:
-            try:
-                result += int(num)
-            except:
-                await self.bot.say("Numbers only please 2")
-                break
-        await self.bot.say("{} = {}".format((' + '.join(map(str, list(nums)))), result))
+    async def Calc(self, *string):
+        allowable_chars = ["(", ")", "+", "-", "/", "*", "//", "%", "[", "]", "{", "}"]
+        string = "".join(string)
 
+        for char in list(string):
+            if char not in allowable_chars and char.isdigit() is False:
+                await bot.say("Not acceptable characters used, try an equal this time please")
+                return
 
-    @commands.command(pass_context=False, no_pm=True)
-    async def sub(self, *nums):
-        result = int(nums[0]) * 2
-        for num in nums:
-            print(num)
-            try:
-                result -= int(num)
-            except:
-                await self.bot.say("Numbers only please 2")
-                break
-        await self.bot.say("{} = {}".format((' - '.join(map(str, list(nums)))), result))
+        string = "".join(string)
+        await bot.say(eval(string))
+
 
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'), description='A playlist example for discord.py')
 bot.add_cog(John_Bot(bot))
